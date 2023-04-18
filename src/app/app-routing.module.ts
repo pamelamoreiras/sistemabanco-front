2885182c6f0942c9478e14a4e5908f9fd21a95a0
Pamela Moreira <pamelamoreiras99@gmail.com>
2023-04-18 15:31:01 -0300

    HTML e CSS para componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { NavigateComponent } from './components/navigate/navigate.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavigateComponent
-  },
-  
+    path: '', component: NavigateComponent, children: [
+      {
+        path: 'home', component: HomeComponent
+      },
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/components/navigate/navigate.component.ts
+++ src/app/components/navigate/navigate.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigate',
   templateUrl: './navigate.component.html',
   styleUrls: ['./navigate.component.css']
 })
-export class NavigateComponent {
+export class NavigateComponent implements OnInit{
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.navigate(['home'])
+  }
 
 }
